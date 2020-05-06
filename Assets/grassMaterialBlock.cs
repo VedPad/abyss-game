2cb1bd3b0eb6458a7ae70213247482d555fc77c1
Vedanth P <vedpadu@gmail.com>
2020-05-06 09:42:29 -0700

    started working on terrain ideas with placeholder assets

---
+++ Assets/grassMaterialBlock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grassMaterialBlock : MonoBehaviour
+{
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    public float strength;
+
+    public float lerpVal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+        _renderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetFloat("_WindDensity", 0.06f);
+        _renderer.SetPropertyBlock(_propBlock);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // _renderer.SetPropertyBlock(_propBlock);
+        //_propBlock.SetFloat("_WindStrength", strength);
+        if (Mathf.Abs(strength) > 0.05f)
+        {
+            strength *= lerpVal;
+            _propBlock.SetFloat("_WindStrength", Mathf.Clamp(strength, -3, 3));
+            _renderer.SetPropertyBlock(_propBlock);
+            
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Mathf.Abs(strength) <= 2f)
+        {
+            Rigidbody2D rB = other.attachedRigidbody;
+            if (rB != null)
+            {
+                strength +=Mathf.Clamp(Mathf.Sign(-rB.velocity.x), -1, 1);
+            }
+        }
+       
+    }
+}
